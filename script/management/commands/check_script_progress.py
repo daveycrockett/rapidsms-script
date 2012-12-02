321c3f583587fabe4954eb57b9233016791dcea2
David McCann <dmccann@keywcorp.com>
2012-12-02 16:36:52 -0500

    early and late params are optional

--- script/management/commands/check_script_progress.py
+++ script/management/commands/check_script_progress.py
@@ -1,24 +1,16 @@
-import datetime
 import logging
-import itertools
-from logging import  handlers
+from logging import handlers
 from django.core.management.base import BaseCommand
 import traceback
-from rapidsms.models import Contact, Connection, Backend
-
-from rapidsms_httprouter.models import Message
 
 from django.db import transaction
 
-from rapidsms.messages.outgoing import OutgoingMessage
-
 from script.utils.outgoing import check_progress
-from script.models import ScriptProgress, Email, Script
+from script.models import Script
 from optparse import OptionParser, make_option
-import datetime
+from datetime import datetime
 from django.core.mail import send_mail
 from django.conf import settings
-from django.template import Context, Template
 
 # Make sure a NullHandler is available
 # This was added in Python 2.7/3.2
@@ -45,18 +37,19 @@ class Command(BaseCommand):
     @transaction.commit_manually
     def handle(self, **options):
 
-        current = datetime.datetime.now()
+        current = datetime.now()
         recipients = getattr(settings, 'ADMINS', None)
         if recipients:
-            recipients = [email for name, email in recipients]
-        if current.hour in range(int(options['e']), int(options['l'])):
-            try:
-                for script in Script.objects.filter(enabled=True):
-                    check_progress(script)
-                transaction.commit()
-            except Exception, exc:
-                transaction.rollback()
-                print traceback.format_exc(exc)
-                logger.debug(str(exc))
-                if recipients:
-                    send_mail('[Django] Error: check_script_progress cron', str(traceback.format_exc(exc)), 'root@uganda.rapidsms.org', recipients, fail_silently=True)
+            recipients = [admintuple[1] for admintuple in recipients]
+        if 'e' in options and 'l' in options and not (current.hour in range(int(options['e']), int(options['l']))):
+            return
+        try:
+            for script in Script.objects.filter(enabled=True):
+                check_progress(script)
+            transaction.commit()
+        except Exception, exc:
+            transaction.rollback()
+            print traceback.format_exc(exc)
+            logger.debug(str(exc))
+            if recipients:
+                send_mail('[Django] Error: check_script_progress cron', str(traceback.format_exc(exc)), 'root@uganda.rapidsms.org', recipients, fail_silently=True)
